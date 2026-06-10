541b336065a8ace817eb7ea326affad671ba899d
luciosantos-Mec <luciosantos@mec.gov.br>
2026-06-10 17:57:42 -0300

    Typst
    Limpeza de repositórios não usados

--- school-report-python/reports/ATS-ORC-R/queries/id_uo_by_sigla.sql
+++
@@ -1,9 +0,0 @@
--- Resolve sigla da universidade para id_unidade_orcamentaria
--- Parâmetro: @sigla (STRING)
-SELECT
-  id_unidade_orcamentaria,
-  unidade_orcamentaria AS nome_universidade,
-  sigla
-FROM `br-mec-segape.educacao_politica_tesouro.tesouro_orcamento_universidade_federal_grupo`
-WHERE UPPER(sigla) = UPPER(@sigla)
-LIMIT 1
--- school-report-python/reports/ATS-ORC-R/queries/universidade_by_sigla.sql
+++
@@ -1,8 +0,0 @@
--- Busca nome da universidade pelo id_unidade_orcamentaria
--- Parâmetro: @id_unidade_orcamentaria (INT64)
-SELECT
-  id_unidade_orcamentaria,
-  unidade_orcamentaria AS nome_universidade
-FROM `br-mec-segape.educacao_politica_tesouro.tesouro_orcamento_universidade_federal_grupo`
-WHERE id_unidade_orcamentaria = @id_unidade_orcamentaria
-LIMIT 1
